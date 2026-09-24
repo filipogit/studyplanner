3e7c7fad007de8953419c48a1e8f10abbaeca881
filipogit <andersson1_filip@hotmail.com>
2026-09-24 16:45:42 +0200

    Lägg till startprofil för mobilappen som lyssnar på lokalt nätverk och tillåt Expo web i CORS

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:8081")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
